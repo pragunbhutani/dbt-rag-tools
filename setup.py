af2430b7206e75d3440055f26139379c2ebb98ab
PRAGUN_BHUTANI <1109752+pragunbhutani@users.noreply.github.com>
2024-02-08 20:40:23 +0530

    updated version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="dbt-rag-tools",
-    version="0.1.0",
+    version="0.1.1",
     description="A set of utilities to enable RAG for dbt projects.",
     long_description=long_description,
     long_description_content_type="text/markdown",
