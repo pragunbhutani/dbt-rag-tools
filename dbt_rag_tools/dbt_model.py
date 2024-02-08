3548bfba3fc620bd25f8eea6616773952fb4fb26
PRAGUN_BHUTANI <1109752+pragunbhutani@users.noreply.github.com>
2024-02-08 21:04:34 +0530

    fixed type imports

--- dbt_rag_tools/dbt_model.py
+++ dbt_rag_tools/dbt_model.py
@@ -1,5 +1,14 @@
-from typing import Callable
-from custom_types import DbtModelDict
+from typing import TypedDict, Callable
+from typing_extensions import NotRequired
+
+class DbtModelColumn(TypedDict):
+    name: str
+    description: NotRequired[str]
+
+class DbtModelDict(TypedDict):
+    name: str
+    description: NotRequired[str]
+    columns: list[DbtModelColumn]
 
 class DbtModel:
     """
